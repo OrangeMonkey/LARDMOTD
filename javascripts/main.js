4462f5951c8912d5610199621f4e09807668dabd
Metapyziks <metapyziks@gmail.com>
2014-02-15 01:04:12 +0000

    Basic JS

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+var protips = [
+    "Right click to throw your knife for an impressive long-range kill",
+    "Don't eat the yellow snow"
+];
+
+window.onload = function() {
+    var motd = document.getElementById("motd");
+    motd.innerHTML = "Protip: " + protips[Math.floor(Math.random() * protips.length)];
+};
